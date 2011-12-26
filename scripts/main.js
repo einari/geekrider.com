05cb1f86a547e7e74d172590171ebf75061381d7
Einar Ingebrigtsen <einari@Einar-Ingebrigtsens-iMac.local>
2011-12-26 13:08:14 +0100

    Starting fleshing out the structure

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+require.config({
+	appDir: "../",
+	paths: {
+		jQuery : "Scripts/jquery-1.7.1.min.js",
+		knockout : "Scripts/knockout-2.0.0.js"
+	}
+});
---
+++ spec/buster.js
@@ -0,0 +1,10 @@
+var config = module.exports;
+
+config["My tests"] = {
+	environment: "node",
+	sources: [
+	],
+	tests: [
+		"*.spec.js"
+	]
+}
