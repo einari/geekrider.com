a18316564f230c1a0a6e1c96792aa4a29d8df9dd
Einar Ingebrigtsen <einari@Einar-Ingebrigtsens-iMac.local>
2011-12-30 00:01:08 +0100

    Working on fleshing out the first feature - Home

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,38 @@
 require.config({
 	appDir: "../",
+	baseUrl: "./scripts",
+	optimize: "none",
+
 	paths: {
-		jQuery : "Scripts/jquery-1.7.1.min.js",
-		knockout : "Scripts/knockout-2.0.0.js"
+		"jquery" : "jquery-1.7.1.min",
+		"knockout" : "knockout-2.0.0"
 	}
 });
+
+define("Home", ["text!../Features/Home/view.html!strip", "text!../Features/Home/view.css", "../Features/Home/viewModel"], function(v,s){
+	return {
+		view: v,
+		styles: s
+	}
+});
+
+
+require(["jquery"], function($) {
+
+	$(function() {
+		var something = $("#something");
+		console.log("We're here");
+	});
+	
+	console.log("Hello world");
+	
+});
+
+require(["Home"], function(h) {
+	console.log("Home");
+});
+
+/*
+require(["Home","text!Home/view.html!strip"], function(h, html) {
+	console.log("Home");
+});*/
