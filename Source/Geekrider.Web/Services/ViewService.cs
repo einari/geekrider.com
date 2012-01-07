dddf88090e20d261e9fc8e799e33717ea2ae71cf
Einar Ingebrigtsen <einar@dolittle.com>
2012-01-07 01:29:02 +0100

    Bifrost up and running with a test service

---
+++ Source/Geekrider.Web/Services/ViewService.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.Collections.Generic;
+using Bifrost.Entities;
+using Bifrost.Events;
+using System.ServiceModel.Web;
+using Bifrost.Serialization;
+
+namespace Geekrider.Services
+{
+    [ServiceContract]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
+    public class ViewService
+    {
+        IEntityContext<EventHolder> _eventRepository;
+        ISerializer _serializer;
+
+        public ViewService(IEntityContext<EventHolder> eventRepository, ISerializer serializer)
+        {
+            _eventRepository = eventRepository;
+            _serializer = serializer;
+        }
+
+
+        [WebGet(ResponseFormat=WebMessageFormat.Json)]
+        public string GetAll()
+        {
+            var events = _eventRepository.Entities.ToArray();
+            var serialized = _serializer.ToJson(events);
+            return serialized;
+
+        }
+    }
+}
