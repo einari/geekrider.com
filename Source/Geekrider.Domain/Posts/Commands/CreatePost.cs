193fba2206b2f368e7de72abac6574bf5e3b6876
Einar Ingebrigtsen <einar@dolittle.com>
2012-01-11 22:22:12 +0100

    Fleshing out Domain, Events and Views

---
+++ Source/Geekrider.Domain/Posts/Commands/CreatePost.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Bifrost.Commands;
+
+namespace Geekrider.Domain.Posts.Commands
+{
+    public class CreatePost : Command
+    {
+        public Guid PostId { get; set; }
+
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
---
+++ Source/Geekrider.Events/Posts/PostCreated.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Bifrost.Events;
+
+namespace Geekrider.Events.Posts
+{
+    public class PostCreated : Event
+    {
+        public PostCreated(Guid eventSourceId) : base(eventSourceId) { }
+
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
