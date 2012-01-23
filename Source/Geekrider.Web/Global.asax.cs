394ef5f41106c6ab2fb193b51d3ae4c48d0ec9eb
Einar Ingebrigtsen <einar@dolittle.com>
2012-01-23 03:28:37 +0100

    Switching to Microsofts Unity since Ninject is crashing and burning like mad on Mono

--- Source/Geekrider.Web/Global.asax.cs
+++ Source/Geekrider.Web/Global.asax.cs
@@ -1,12 +1,12 @@
 using System.Web.Routing;
 using Bifrost.Configuration;
 using Bifrost.Execution;
-using Bifrost.Ninject;
 using Bifrost.Services.Commands;
 using Bifrost.Services.Execution;
 using Bifrost.Web.Mvc;
 using Geekrider.Services;
-using Ninject;
+using Microsoft.Practices.Unity;
+using Bifrost.Unity;
 
 namespace Geekrider
 {
@@ -27,8 +27,8 @@ namespace Geekrider
 
         protected override IContainer CreateContainer()
         {
-            var kernel = new StandardKernel();
-            var container = new Container(kernel);
+            var unity = new UnityContainer();
+            var container = new Container(unity);
             return container;
         }
     }
